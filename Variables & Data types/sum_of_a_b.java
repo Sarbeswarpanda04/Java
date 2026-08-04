8128355e2cf38684f42ce8627aef38ab5547839f
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-08-04 15:44:51 +0530

    add sum_of_a_b.java to demonstrate basic addition in Java

---
+++ Variables & Data types/sum_of_a_b.java
@@ -0,0 +1,8 @@
+public class sum_of_a_b {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
