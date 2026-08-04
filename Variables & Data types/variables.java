fc2fadcd166064e6324e280259ad5ee233490ddb
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-08-04 15:28:44 +0530

    add variables and data types example

---
+++ Variables & Data types/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int age = 20;
+        double height = 5.9;
+        String name = "Sarbeswar Panda";
+        boolean isStudent = true;
+
+        // Printing variable values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
