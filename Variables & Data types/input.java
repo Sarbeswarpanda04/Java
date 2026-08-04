599544a738281a58c58f5051dce3669d6dfb9729
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-08-04 16:01:13 +0530

    refactor input.java to improve variable naming and streamline user input handling

--- Variables & Data types/input.java
+++ Variables & Data types/input.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Enter Name : ");
-        String input = sc.nextLine();
-        System.out.println(input);
+        String name = sc.nextLine();
+        System.out.print("Enter age : ");
+        int age = sc.nextInt();
+
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
     }
 }
