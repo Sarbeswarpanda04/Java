8e3e694fd8141673b06e7d1f8f4fd406eecf913f
Sarbeswarpanda04 <sarbeswarpanda143@gmail.com>
2026-08-04 16:09:26 +0530

    update sum_of_a_b.java to replace hardcoded values with user input for dynamic addition

--- Variables & Data types/sum_of_a_b.java
+++ Variables & Data types/sum_of_a_b.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class sum_of_a_b {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int sum = a + b;
-        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+        // int a = 5;
+        // int b = 10;
+        // int sum = a + b;
+        // System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter 1st number : ");
+        int num2 = sc.nextInt();
+
+        int sum = num1+num2;
+
+        System.out.print("Sum of "+num1+" and "+num2+" = "+sum);
     }
 }
